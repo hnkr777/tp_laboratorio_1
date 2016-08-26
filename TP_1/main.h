0707b7272dc09b8c9708f4a5f1b72354cb773789
Fede <hnkr7@hotmail.com>
2016-08-26 04:16:23 -0300

    Update main.h

--- TP_1/main.h
+++ TP_1/main.h
@@ -1,12 +1,9 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-
-
-#endif
+#ifndef MAIN_H
+#define MAIN_H
 
 
 int menu(int operand1, int operand2, char flag);
 int loadOperator();
 void showOperation(int operand1, int operand2, char flag);
 
+#endif
