7a8999c271e0bc1c1dd2fbfb6930c5042ad71f24
Fede <hnkr7@hotmail.com>
2016-08-30 04:23:25 -0300

    Update funciones.h

--- TP_1/funciones.h
+++ TP_1/funciones.h
@@ -2,14 +2,10 @@
 #define FUNCIONES_H
 
 
-
-#include "funciones.c"
-
 int addition(int operand1, int operand2);
 int subtraction(int operand1, int operand2);
 int multiplication(int operand1, int operand2);
 int division(int operand1, int operand2);
 int factorial(int operand1);
 
-
-#endif
+#endif // FUNCIONES_H
